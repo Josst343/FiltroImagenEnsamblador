064c25e150d92b9cbc5e09000ba5f9f9869b809a
Josst343 <43652757+Josst343@users.noreply.github.com>
2019-05-29 21:20:32 -0500

    Corrigiendo errores de ciclos ya crea una imagen concreta a 16 bits

--- Source.asm
+++ Source.asm
@@ -1,21 +1,21 @@
-
+.586
 .model flat, stdcall
 .stack  1024
-
+.xmm
 ;Prototipos de las funciones
 ExitProcess PROTO, :DWORD
 CreateFileA PROTO, :DWORD,:DWORD,:DWORD,:DWORD,:DWORD,:DWORD,:DWORD
 WriteFile PROTO, :DWORD,:DWORD,:DWORD,:DWORD,:DWORD
 ReadFile PROTO, :DWORD,:DWORD,:DWORD,:DWORD,:DWORD
 .data 
-nombre db "imagenPruebaEscalada.raw",0
+nombre db "imagen16Mono.raw",0
 nombre1 db "imgPruebaBin.raw",0
 handle dd ?
 conts db 16 dup(9)
-imagen dd 106500 dup(?)
-imagenProm dd 106500 dup(?)
+imagen dd 213000 dup(?)
+imagenProm dd 213000 dup(?)
 
-cc1 dd 21
+cc1 dd 86
 cc2 dd 297
 
 .code
@@ -26,7 +26,9 @@ main PROC
 	MOV EDI,0
 	MOV ECX,cc2
 	
-	cicloColumnas:	push ECX
+	cicloColumnas:
+	push ECX
+
 	mov ECX,cc1	
 	cicloFilas:
 	mov EBX,EDI
@@ -36,14 +38,14 @@ main PROC
 	MOVUPS XMM2,OWORD PTR imagen[EBX*8+2]
 	addps  xmm0 ,  xmm1 
 	ADDPS XMM0,XMM2
-	ADD EBX,355	
+	ADD EBX,710	
 	MOVUPS XMM3, OWORD PTR imagen[EBX*8]
 	MOVUPS XMM1,OWORD PTR imagen[EBX*8+1]
 	MOVUPS XMM2,OWORD PTR imagen[EBX*8+2]	
 	ADDPS XMM0,XMM3
 	ADDPS XMM0,XMM2
 	ADDPS XMM0,XMM1
-	ADD EBX,710
+	ADD EBX,1420
 	MOVUPS XMM3, OWORD PTR imagen[EBX*8]
 	MOVUPS XMM1,OWORD PTR imagen[EBX*8+1]
 	MOVUPS XMM2,OWORD PTR imagen[EBX*8+2]
@@ -54,10 +56,14 @@ main PROC
 
 	MOVUPS OWORD PTR imagenProm[EDI*8],XMM0
 	inc EDI
-
+	DEC ECX
+	JNE cicloFilas
+	;en lugar de usar loop usar decremento con salto condicional ""go to
 	;loop cicloFilas	
 	pop ECX
-;	loop cicloColumnas
+	;loop cicloColumnas
+	DEC ECX
+	JNE cicloColumnas
 
 	call creaArchivo
 	call escribirArchivo
@@ -99,7 +105,7 @@ abrirArchivo endp
 escribirArchivo proc
 	push 0
 	push 0
-	push 106500 ;Numero de caracteres a escribir
+	push 213000 ;Numero de caracteres a escribir
 	push offset imagenProm
 	push handle
 	call WriteFile
@@ -111,10 +117,9 @@ leerArchivo proc
 	call abrirArchivo
 	push 0
 	push 0
-	push 106500
+	push 213000
 	push offset imagen
 	push handle
 	call ReadFile
 ret
 leerArchivo endp
-END
